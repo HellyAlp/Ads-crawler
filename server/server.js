b768879bd52f3442ac85464d7ff390d2dfd8a6fb
HellyAlp <117525756+HellyAlp@users.noreply.github.com>
2023-10-01 20:48:08 +0300

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+
+const app = express();
+const port = 5000; 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // Ignore SSL certificate errors because of msn.com site
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/ads-txt/:domain', async (req, res) => {
+    const domain = req.params['domain'];
+    try {
+      const response = await axios.get(`https://${domain}/ads.txt`);
+      const domains = response.data.match(/^(?![\s]*#)[^=,]+/gm).map((line) => line.trim().toLowerCase());
+      const counts = domains.reduce((count, dom) => {
+                count[dom] = (count[dom] || 0) + 1;
+                return count;
+              }, {});
+      res.send(counts);
+      console.log(counts)
+    } catch (error) {
+      res.status(404).send({ error: 'Ads.txt file not found for the specified domain.' });
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+  });
